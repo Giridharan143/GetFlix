c8e726d8f9245b480f1cfbab098430e2af8d6b84
Giridharan143 <krishnamoorthygiri143@gmail.com>
2025-07-11 16:46:38 +0530

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="test-2xl text-green-500">
+      Genflix
+    </h1>
+    </>
+  );
+}
---
+++ pages/tester.tsx
@@ -0,0 +1,10 @@
+
+const tester = () =>{
+  return (
+    <h1> 
+      We are Working on building an Cool Project !!!
+    </h1>
+  )
+}
+
+export default tester;
